4550a9969a777f544306468a2721a6e936f29010
germain-hug <hugo.germain@supelec.fr>
2018-03-03 09:47:12 -0800

    Cleanup

--- models/cgan.py
+++ models/cgan.py
@@ -3,7 +3,7 @@ from utils.utils import z_noise, c_noise, make_trainable, ups_conv_bn
 from utils.visualization import plot_results_CGAN
 from keras.models import Model
 from keras.layers import *
-from keras.optimizers import Adam, RMSprop
+from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 from models.gan import GAN
 from tqdm import tqdm
@@ -72,10 +72,11 @@ class CGAN(GAN):
         """ Generate fake and real data to train D. Both real and fake data
         are conditioned on a one-hot encoded vector c.
         """
-        permutations = np.random.randint(0,X_train.shape[0],size=2*sz)
+        permutations = np.random.randint(0,X_train.shape[0],size=sz)
         real_images  = X_train[permutations[:sz]]
-        labels = to_categorical(y_train[permutations[:2*sz]], 10)
-        X = np.concatenate((real_images, self.G.predict([z_noise(sz),labels[sz:]])))
+        labels = to_categorical(y_train[permutations[:sz]], 10)
+        X = np.concatenate((real_images, self.G.predict([z_noise(sz),labels])))
+        label = np.concatenate((labels, labels))
         return X, [0]*sz + [1]*sz, labels
 
     def generator(self, input_G, conditioning_label):
